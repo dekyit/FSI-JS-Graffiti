c3d8d94d6f5a05440046ffcd3be71df9fc4b50c8
Dekyi Tenzin <dekyitenzin@Dekyis-Air.lan>
2023-03-23 19:58:15 -0400

    idk

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let header = document.querySelector('#page-header');
-header.style.textAlign = "
+header.style.textAlign = "center";
+document.getElementById("Doggos").innerHTML="Old Dogs>"
+header.style.backgroundColor="pink";
 
+let dogImages = document.querySelectorAll ('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.bprderRadius = '50px' 
+}
 
 Select some elements...
